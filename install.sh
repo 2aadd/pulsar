fd0ce0edd52dd43cf3274b84ed5e448b38346eee
Ali <ali@localhost.localdomain>
2026-05-09 15:30:35 +0400

    Fix install script shebang

--- install.sh
+++ install.sh
@@ -1,28 +1,18 @@
-
 #!/bin/bash
 
 echo "Installing Pulsar..."
 
-
 if ! command -v pip3 &> /dev/null; then
-
     echo "pip3 not found, installing..."
-
     sudo dnf install python3-pip -y
-
 fi
-
 
 pip3 install psutil rich --break-system-packages
 
-# pulsar'ı indir
+curl -sSL https://raw.githubusercontent.com/2aadd/pulsar/main/pulsar.py -o /tmp/pulsar.py
 
-curl -sSL https://raw.githubusercontent.com/2aadd/pulsar/main/pulsar.py -o ~/pulsar.py
-
-
-sudo cp ~/pulsar.py /usr/local/bin/pulsar
-
+echo '#!/usr/bin/env python3' | sudo tee /usr/local/bin/pulsar > /dev/null
+sudo cat /tmp/pulsar.py | sudo tee -a /usr/local/bin/pulsar > /dev/null
 sudo chmod +x /usr/local/bin/pulsar
 
 echo "Done! Run with: pulsar"
-
